0ec450948150c5b4c319c91ab110eee4dc9df2b8
matheus de freitas <matheus.f0192@outlook.com>
2023-05-25 21:15:46 -0300

    ADD: método de inserir

--- APICaminhoesCrude/Controllers/CaminhaoController.cs
+++ APICaminhoesCrude/Controllers/CaminhaoController.cs
@@ -1,5 +1,9 @@
 ﻿using APICaminhoesCrude.Repository;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using APICaminhoesCrude.Context;
+using Microsoft.AspNetCore.Http;
 
 namespace c.Controllers
 {
@@ -15,6 +19,86 @@ namespace c.Controllers
             _caminhaoRepository = caminhaoRepository;
         }
 
+        /// <summary>
+        /// Retorna o que corresponde ao Id
+        /// </summary>
+        /// <returns>Caminhao pelo Id</returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCaminhaoId(int id)
+        {
+            if (id == 0)
+            {
+                throw new Exception($"Id não pode ser zero ou nulo.");
+            }
+
+            var caminhao = await _caminhaoRepository.BuscarId(id);
+
+            if (caminhao == null)
+            {
+                throw new Exception("Caminhao não localizado");
+            }
+
+            return Ok(caminhao);
+        }
+
+        /// <summary>
+        /// Retorna a lista completa
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> GetCaminhoes()
+        {
+            var caminhoes = await _caminhaoRepository.Listar();
+            return Ok(caminhoes);
+        }
+
+        /// <summary>
+        /// Insere
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Insert([FromBody] Caminhao caminhao)
+        {
+            caminhao.Id = 0;
+
+            if (caminhao.AnoFabricacao == 0 || caminhao.AnoModelo == 0)
+            {
+                throw new Exception($"Ano de Fabricação e Ano do modelo não podem ser 0 ou inexistentes");
+            }
+
+            if (caminhao.AnoFabricacao != DateTime.Now.Year)
+            {
+                throw new Exception($"Ano de Fabricação não é o atual");
+            }
+
+            if (caminhao.AnoModelo > caminhao.AnoFabricacao + 1 || caminhao.AnoModelo < caminhao.AnoFabricacao)
+            {
+                throw new Exception($"Ano do modelo menor que o ano atual ou maior que o subsequente");
+            }
+
+            var caminhaodb = await _caminhaoRepository.Inserir(caminhao);
+            return new ObjectResult(caminhaodb) { StatusCode = StatusCodes.Status201Created };
+        }
+
+        /// <summary>
+        /// Atualiza o Caminhão através do Id
+        /// </summary>        
+        /// <param name="id"></param>
+        /// <param name="caminhao"></param>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Atualizar([FromRoute] int id, [FromBody] Caminhao caminhao)
+        {
+            if (id == 0)
+            {
+                throw new Exception($"Id não pode ser zero ou nulo.");
+            }
+
+            if (caminhao.AnoModelo > caminhao.AnoFabricacao + 1 || caminhao.AnoModelo < caminhao.AnoFabricacao)
+            {
+                throw new Exception($"Ano do modelo menor que o ano da Fabricação ou maior que um ano a mais dela.");
+            }
+
+            var caminhaodb = await _caminhaoRepository.Atualizar(id, caminhao);
+            return new ObjectResult(caminhaodb) { StatusCode = StatusCodes.Status200OK };
+        }
 
     }
 }
