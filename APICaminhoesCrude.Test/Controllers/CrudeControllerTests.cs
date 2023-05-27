dd40d6d17bd2d776a730b0a57e66ff82d88b3e43
IsaiasGabriel <isaiasgabrielhub@gmail.com>
2023-05-27 12:55:31 -0300

    ADD: Fake Repository e Teste dos metodos Gets

--- APICaminhoesCrude.Test/Controllers/CrudeControllerTests.cs
+++ APICaminhoesCrude.Test/Controllers/CrudeControllerTests.cs
@@ -1,11 +1,14 @@
-﻿using APICaminhoesCrude.Controllers;
+﻿using APICaminhoesCrude.Context;
+using APICaminhoesCrude.Controllers;
 using APICaminhoesCrude.Repository;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace APICaminhoesCrude.Test.Controllers
 {
@@ -18,6 +21,88 @@ namespace APICaminhoesCrude.Test.Controllers
             _caminhaoController = new CaminhaoController(new Mock<ICaminhaoRepository>().Object);
         }
 
+        //Fake Repository
+        public List<Caminhao> CriarRepository()
+        {
+            List<Caminhao> caminhoes = new List<Caminhao>() { new Caminhao
+            {
+                Id = 1,
+                AnoFabricacao = 2022,
+                AnoModelo = 2023,
+                NomeModelo = MarcaModelo.Ford
+            },
+            new Caminhao
+            {
+                Id = 2,
+                AnoFabricacao = 2022,
+                AnoModelo = 2023,
+                NomeModelo = MarcaModelo.Mercedes
+            },
+            new Caminhao
+            {
+                Id = 3,
+                AnoFabricacao = 2022,
+                AnoModelo = 2023,
+                NomeModelo = MarcaModelo.Scania
+            }};
 
+            return caminhoes;
+        }
+
+        #region Gets
+        [Fact]
+        public async Task GetCaminhoesAsync()
+        {
+            var resp = await _caminhaoController.GetCaminhoes();
+            var result = resp as ObjectResult;
+
+            if (result.StatusCode == 200)
+            {
+                Assert.True(true);
+            }
+            else
+            {
+                Assert.True(false);
+            }
+        }
+
+        [Fact]
+        public async Task GetCaminhaoId_Idencontrado()
+        {
+            int id = 1;
+
+            var fakeRepository = CriarRepository();
+            var mockRepository = new Mock<ICaminhaoRepository>();
+            mockRepository.Setup(x => x.BuscarId(id)).Returns(Task.FromResult(fakeRepository.FirstOrDefault(u => u.Id == id)));
+            _caminhaoController = new CaminhaoController(mockRepository.Object);
+
+            var resp = await _caminhaoController.GetCaminhaoId(id);
+            var result = resp as ObjectResult;
+
+            if (result.StatusCode == 200)
+            {
+                Assert.True(true);
+            }
+            else
+            {
+                Assert.True(false);
+            }
+        }
+
+        [Fact]
+        public async Task GetCaminhaoId_IdNaoencontrado()
+        {
+            int id = 10000;
+
+            var fakeRepository = CriarRepository();
+            var mockRepository = new Mock<ICaminhaoRepository>();
+            mockRepository.Setup(x => x.BuscarId(id)).Returns(Task.FromResult(fakeRepository.FirstOrDefault(u => u.Id == id)));
+            _caminhaoController = new CaminhaoController(mockRepository.Object);
+
+            await Assert.ThrowsAsync<Exception>(async () =>
+            await _caminhaoController.GetCaminhaoId(id));
+        }
+
+        #endregion
     }
 }
